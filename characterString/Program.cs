0018d6d374f2c7ac5470b685195e6cbd133ad9b4
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-09-16 11:43:06 +0530

    Add files via upload

---
+++ characterString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace characterString
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = "hello";
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (!charCount.TryAdd(c, 1)) 
+                {
+                    charCount[c]++;
+                }
+            }
+
+            foreach (var pair in charCount)
+            {
+                Console.WriteLine($"'{pair.Key}': {pair.Value}");
+            }
+        }
+    }
+
+}
