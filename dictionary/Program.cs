c8079a3c576b44506b2fd12fc064b7c963951c55
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-09-19 11:48:19 +0530

    Add files via upload

---
+++ dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿ using System;
+ using System.Collections.Generic;
+
+namespace dictionary
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = { "apple", "banana", "apple", "orange", "banana", "apple" };
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (!wordCount.TryAdd(word,1))
+                {
+                    wordCount[word]++;
+                }
+
+                //if (wordCount.ContainsKey(word))
+                //{
+                //    wordCount[word]++;
+                //}
+                //else
+                //{
+                //    wordCount[word] = 1;
+                //}
+            }
+
+            foreach (var pair in wordCount)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
