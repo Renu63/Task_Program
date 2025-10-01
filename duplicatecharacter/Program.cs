57389fe11293356dd2fa7903e54f5b17ae88d5ac
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-01 11:25:52 +0530

    Add files via upload

---
+++ duplicatecharacter/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace duplicatecharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+            IEnumerable<char> result = Enumerable.Empty<char>();
+
+            for (int i= 0; i < str.Length; i++) 
+                {
+                if (!result.Contains(str[i]))
+                {
+                    result = result.Append(str[i]);
+                }
+            }
+
+            Console.WriteLine("String without duplicate characters: ");
+            Console.WriteLine(string.Join(" ", result));
+
+            Console.ReadKey();
+        }
+    }
+}
+  
