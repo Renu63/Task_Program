066a58be99beeef2faa2690625c4b624c10c81b2
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-06 12:11:39 +0530

    Add files via upload

---
+++ factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number of elements to Print : ");
+            int num = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            { 
+                factorial *= i;
+
+                Console.WriteLine(factorial);
+
+            }
+        }
+    }
+}
+
