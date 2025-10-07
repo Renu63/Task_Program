f7ef8e02aea389ac07b25103cc743d9cf0231220
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-07 16:51:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 0, num2 = 1, num3;
+            Console.WriteLine("Enter the number of elements to Print : ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
