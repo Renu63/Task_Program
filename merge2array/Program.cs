a2cbc80a86e9c40ace534d02609e12460c353b20
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-08 10:50:54 +0530

    Add files via upload

---
+++ merge2array/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace merge2array
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                int[] arr1 = { 11, 12, 3 };
+                int[] arr2 = { 14, 5, 16 };
+
+                int[] mergeArray = new int[arr1.Length + arr2.Length];
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    mergeArray[i] = arr1[i];
+                }
+
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    mergeArray[arr1.Length + i] = arr2[i];
+                }
+
+                Console.WriteLine("Merge Array:");
+                for (int i = 0; i < mergeArray.Length; i++)
+                {
+                    Console.Write(mergeArray[i] + " ");
+                }
+
+                Console.ReadKey(); 
+            }
+    }
+
+}
