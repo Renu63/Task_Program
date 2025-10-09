8e9369105c4255b66b7dd9e49b996e2a13a8d0ff
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-09 11:27:33 +0530

    Add files via upload

---
+++ numlarsmall/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace numlarsmall
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 22, 13, 25, 69, 84, 0 };
+            int maxnum = arr[0];
+            int minnum = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > maxnum)
+                {
+                    maxnum = arr[i];
+                }
+                else if (arr[i] < minnum)
+                { 
+                    minnum = arr[i];
+                }
+            }
+
+            Console.WriteLine("the largest number is: " + maxnum);
+            Console.WriteLine("the smallest number is: " + minnum);
+        }
+    }
+}
