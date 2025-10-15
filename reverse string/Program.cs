d4678873d6f634e67d1eef410465031dcd7bc665
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-15 19:01:22 +0530

    Add files via upload

---
+++ reverse string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class reverseString
+{
+    static string reversestring(string s)
+    {
+
+        Console.Write("Enter a string to Check Palindrome : ");
+        string name = Console.ReadLine();
+        StringBuilder res = new StringBuilder();
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            res.Append(s[i]);
+        }
+
+        return res.ToString();
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "uhuhty";
+        string res = reversestring(s);
+        Console.WriteLine(res);
+    }
+}
