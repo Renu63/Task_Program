6e593310d1c2ce4ad7fbf871f879e49eca495053
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-10-16 17:58:02 +0530

    Add files via upload

---
+++ reverseArray/Program.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+
+namespace reverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            Console.Write(" Reversed array: ");
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
