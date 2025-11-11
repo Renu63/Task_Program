84ec4447a9a89cba49e04ae277b468cc4e99a662
Renuka kale <85407443+Renu63@users.noreply.github.com>
2025-11-11 11:10:18 +0530

    Add files via upload

---
+++ sortascdesc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace sortascdes
+{
+    class Program
+    {
+        static void Main()
+        {
+        
+            int[] arr = { 9, 2, 4, 3, 1, 5 };
+            Console.WriteLine("Original array:");
+            PrintArray(arr);
+
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Ascending order:");
+            PrintArray(arr);
+
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Descending order:");
+            PrintArray(arr);
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine(); 
+        }
+    }
+
+}
+
